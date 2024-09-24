2e8e6f7c457b40b963e2682da31ff56d9c1cd235
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2024-09-24 09:37:26 +0200

    Removed unused imports

--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,11 +1,10 @@
 
 #[cfg(test)]
 mod tests {
-    use std::{borrow::Borrow, collections::HashMap, path::PathBuf};
+    use std::path::PathBuf;
 
     use helm_chart_version_bumper::find_valid_yaml_files;
 
-    use super::*;
 
     #[test]
     fn test_find_valid_yaml_files_only_returns_valid_files(){
